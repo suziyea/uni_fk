3d64dbf95f77d3b886973527abf98418b10308ae
houakang <houakang@126.com>
2022-04-08 13:44:18 +0800

    feat: 添加我的页面

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,9 +1,18 @@
 <template>
 	<view class="container">
-		<view class="top">
-			<view class="user">
-				<view class="avatar"><image src="/static/icons/default-avatar.png" mode="aspectFill"></image></view>
-				<view class="right" v-if="isLogin">
+		<view class="bgBox">
+			<view class="userInfo">
+				<view class="avatar">
+					<image src="/static/icon/head.png" mode="aspectFill"></image>
+				</view>
+				<view class="userName" v-if="isLogin">
+					17701140105
+				</view>
+				<view class="userName" v-else>
+					登录
+				</view>
+				
+				<!-- <view class="right" v-if="isLogin">
 					<view class="nickname">
 						<view>{{realName}}</view>
 						<view class="star" v-if="isMember">
@@ -15,71 +24,35 @@
 					</view>
 					<view class="account">账号：{{account}}</view>
 				</view>
-				<navigator v-else class="login" open-type="navigate" url="/pages/login/login">登录</navigator>
+				<navigator v-else class="login" open-type="navigate" url="/pages/login/login">登录</navigator> -->
 			</view>
 		</view>
-		<view class="order">
-			<view class="title"></view>
-			<view class="nav">
-				<view class="items" v-for="(item, index) in navs" :key="index">
-					<view class="icon" @click.stop="orderPage(item)">
-						<image :src="item.icon" mode="aspectFill"></image>
-					</view>
-					<view class="name">{{item.name}}</view>
-				</view>
+		
+		
+		<view class="memberLegal">
+			<view class="memberEnjoy">
+				<u-row justify="space-between">
+					<u-col span="4" justify="center" v-for="(item,index) in memberNav">
+						<view class="demo-layout bg-purple iconList">
+							<image :src="item.icon" mode="aspectFill" @click="clickNav(item)"></image>
+							<view class="title">{{item.name}}</view>
+						</view>
+					</u-col>
+				</u-row>
 			</view>
 		</view>
-		<view class="list">
-			<navigator open-type="navigate" url="/pages/my/card/card" class="item">
-				<view class="item_icon">
-					<image src="/static/icons/my_card.png" mode="aspectFill"></image>
-				</view>
-				<view class="title">我的银行卡</view>
-				<view class="arrow"><image src="/static/icons/arrow-icon.png" mode="aspectFill"></image></view>
-			</navigator>
-			<navigator class="item" open-type="navigate" url="/pages/my/my-rights/my-rights">
-				<view class="item_icon">
-					<image src="/static/icons/read.png" mode="aspectFill"></image>
-				</view>
-				<view class="title">我的权益</view>
-				<view class="arrow"><image src="/static/icons/arrow-icon.png" mode="aspectFill"></image></view>
-			</navigator>
-			<navigator class="item" open-type="navigate" url="/pages/service/service">
-				<view class="item_icon">
-					<image src="/static/icons/service.png" mode="aspectFill"></image>
-				</view>
-				<view class="title">客服中心</view>
-				<view class="arrow"><image src="/static/icons/arrow-icon.png" mode="aspectFill"></image></view>
-			</navigator>
-			<navigator class="item" open-type="navigate" url="/pages/my/agreement-list/agreement-list">
-				<view class="item_icon">
-					<image src="/static/icons/read.png" mode="aspectFill"></image>
-				</view>
-				<view class="title">查看协议</view>
-				<view class="arrow"><image src="/static/icons/arrow-icon.png" mode="aspectFill"></image></view>
-			</navigator>
-			<navigator class="item" open-type="navigate" url="/pages/my/about-us/about-us">
-				<view class="item_icon">
-					<image src="/static/icons/aboutus.png" mode="aspectFill"></image>
-				</view>
-				<view class="title">关于我们</view>
-				<view class="arrow"><image src="/static/icons/arrow-icon.png" mode="aspectFill"></image></view>
-			</navigator>
-			<navigator v-if="isLogin" class="item" open-type="navigate" url="/pages/login/unregister/unregister">
-				<view class="item_icon">
-					<image src="/static/icons/unreg_icon.png" mode="aspectFill"></image>
-				</view>
-				<view class="title">注销账户</view>
-				<view class="arrow"><image src="/static/icons/arrow-icon.png" mode="aspectFill"></image></view>
-			</navigator>
-			<view v-if="isLogin" class="item" @click.stop="logout">
-				<view class="item_icon">
-					<image src="/static/icons/logout.png" mode="aspectFill"></image>
-				</view>
-				<view class="title">退出登录</view>
-				<view class="arrow"><image src="/static/icons/arrow-icon.png" mode="aspectFill"></image></view>
-			</view>
+		
+		<view class="listView">
+			<u-cell-group>
+				<u-cell v-for="(item,index) in cellList"
+				    :title="item.name"
+					size="large"
+				    isLink
+				    :icon="item.icon"
+				></u-cell>
+			</u-cell-group>
 		</view>
+		
 	</view>
 </template>
 
@@ -91,24 +64,40 @@
 	export default {
 		data() {
 			return {
-				navs: [{
-					icon: '/static/icons/order_all.png',
-					path: '/pages/my/my-order/my-order?tab=0',
-					name: '全部'
-				},{
-					icon: '/static/icons/card.png',
-					path: '/pages/my/my-order/my-order?tab=1',
-					name: '待支付'
-				},{
-					icon: '/static/icons/complete.png',
-					path: '/pages/my/my-order/my-order?tab=2',
-					name: '已完成'
-				}
-				// ,{
-				// 	icon: '/static/icons/refund.png',
-				// 	path: '/pages/my/my-order/my-order?tab=3',
-				// 	name: '退款'
-				// },
+				isLogin: true,
+				memberNav: [{
+						icon: '/static/icon/memberLegal.png',
+						path: '',
+						name: '会员权益'
+					}, {
+						icon: '/static/icon/myLegal.png',
+						path: '',
+						name: '我的权益'
+					}, {
+						icon: '/static/icon/kefu.png',
+						path: '',
+						name: '客服中心'
+					}
+				],
+				// cellList 
+				cellList: [{
+						icon: '/static/icon/look_agreement.png',
+						path: '',
+						name: '查看协议'
+					}, {
+						icon: '/static/icon/about_us.png',
+						path: '',
+						name: '关于我们'
+					}, {
+						icon: '/static/icon/logoff.png',
+						path: '',
+						name: '注销账户'
+					},
+					{
+						icon: '/static/icon/logout.png',
+						path: '',
+						name: '退出登录'
+					}
 				]
 			};
 		},
@@ -124,11 +113,14 @@
 				uni.switchTab({
 					url: '/pages/index/index'
 				})
-			}
+			},
+			clickNav(item) {
+				
+			},
 		},
 		onLoad() {
 			uni.showLoading({
-			
+
 			})
 		},
 		onReady() {
@@ -152,53 +144,130 @@
 <style lang="scss" scoped>
 	.container {
 		width: 100%;
-		height: 100%;
-		// background-color: #f7f7f7;
+		height: 100vh;
+		background-color: #f7f7f7;
 	}
+	.bgBox {
+		width: 100%;
+		height: 200px;
+		background: url(../../static/img/my_bg.png) no-repeat;
+		background-size: cover;
+		position: relative;
+		.userInfo {
+			position: absolute;
+			top: 82px;
+			left: 30px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			.avatar {
+				width: 50px;
+				height: 50px;
+				image {
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.userName {
+				font-size: 14px;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #FFFFFF;
+				line-height: 20px;
+				margin: 0 13px;
+			}
+		}
+		
+	}
+	
+	.memberLegal {
+		padding: 0 16px;
+		.memberEnjoy {
+			width: 343px;
+			height: 84px;
+			background: #FFFFFF;
+			box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.07);
+			border-radius: 4px;
+			margin-top: -47px;
+			position: relative;
+			z-index: 21;
+			display: flex;
+			.iconList {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				image {
+					width: 36px;
+					height: 36px;
+				}
+				.title {
+					font-size: 12px;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #656565;
+					line-height: 17px;
+				}
+			}
+		}
+		.listView {
+		}
+		
+	}
+
 	.top {
 		width: 100%;
-		height: 421upx;
-		background: url(../../static/bgs/my_top.png) no-repeat;
+		height: 200px;
+		background: url(../../static/img/my_bg.png) no-repeat;
 		background-size: cover;
-		.user{
+
+		.user {
 			position: absolute;
 			@extend %flex-layout-row;
 			justify-content: flex-start;
 			top: 119upx;
 			left: 50upx;
-			.avatar{
+
+			.avatar {
 				flex: 0 0 96upx;
 				width: 96upx;
 				height: 96upx;
-				image{
+
+				image {
 					width: 100%;
 					height: 100%;
 				}
 			}
-			.right{
+
+			.right {
 				flex: 1;
 				margin-left: 24upx;
-				.nickname{
+
+				.nickname {
 					font-size: 32upx;
 					color: #FFFFFF;
 					@extend %flex-layout-row;
 					justify-content: flex-start;
 					align-items: baseline;
-					.star{
+
+					.star {
 						width: 23upx;
 						height: 21upx;
 						margin-left: 8upx;
-						image{
+
+						image {
 							width: 100%;
 							height: 100%;
 						}
 					}
 				}
-				.account{
+
+				.account {
 					font-size: 24upx;
 					color: #FFFFFF;
 				}
 			}
+
 			.login {
 				font-size: 32upx;
 				color: #FFFFFF;
@@ -206,73 +275,95 @@
 			}
 		}
 	}
-	.order{
+
+	.order {
 		width: 699upx;
 		height: 250upx;
 		background-color: #FFFFFF;
 		margin: -160upx auto 0 auto;
 		border-radius: 14upx;
+
 		.title {
 			height: 30upx;
 			font-size: 30upx;
 			color: #FFFFFF;
 		}
-		.nav{
+
+		.nav {
 			@extend %flex-layout-row;
 			justify-content: space-between;
-			.items{
+
+			.items {
 				width: 32%;
 				text-align: center;
-				.icon{
+
+				.icon {
 					width: 112upx;
 					height: 112upx;
 					margin: 0 auto;
-					image{
+
+					image {
 						width: 100%;
 						height: 100%;
 					}
 				}
-				.name{
+
+				.name {
 					padding-top: 5upx;
 					font-size: 26upx;
 					color: $mc-normal-font-color;
 				}
 			}
-		}	
+		}
 	}
-	.list{
+
+	.list {
 		background-color: #FFFFFF;
 		border-radius: 14upx;
 		margin: 32upx 26upx 0 26upx;
-		.item{
+
+		.item {
 			@extend %flex-layout-row;
 			justify-content: space-between;
 			padding: 29upx 26upx;
 			align-items: center;
-			.item_icon{
+
+			.item_icon {
 				flex: 0 0 54upx;
 				width: 54upx;
 				height: 54upx;
-				image{
+
+				image {
 					width: 100%;
 					height: 100%;
 				}
 			}
-			.title{
+
+			.title {
 				flex: 1;
 				margin-left: 19upx;
 				font-size: $mc-normal-font-size;
 				color: $mc-normal-font-color;
 			}
-			.arrow{
+
+			.arrow {
 				flex: 0 0 54upx;
 				width: 54upx;
 				height: 54upx;
-				image{
+
+				image {
 					width: 100%;
 					height: 100%;
 				}
 			}
 		}
+	}
+	::v-deep .u-row {
+		width: 100%;
+	}
+	/deep/ .u-cell-group {
+		margin: 0 16px;
+		background: #FFFFFF;
+		margin-top: 12px !important;
 	}
 </style>
