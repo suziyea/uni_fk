56848b115aae37089148d3a3e76fb740be79c868
houakang <houakang@126.com>
2022-04-09 14:28:23 +0800

    feat: 更行登录

---
+++ pages/login/login/login.vue
@@ -0,0 +1,236 @@
+<template>
+	<view class="container">
+		<view class="login_bg  u-flex u-flex-items-center u-flex-center">
+			<view class="header u-flex u-flex-items-center u-flex-center">
+				<image src="/static/img/login_head.png" mode="aspectFill"></image>
+			</view>
+		</view>
+
+		<view class="formList">
+			<u--form :model="formContent" :rules="rules" ref="uForm">
+				<u-form-item label="" prop="phone">
+					<u--input v-model="formContent.phone" prefixIcon="phone" ref="item1"
+						prefixIconStyle="font-size: 22px;color: #909399">
+					</u--input>
+				</u-form-item>
+
+				<u-form-item label="" prop="smsCode" ref="item1">
+					<!-- <u--input  v-model="phone" border suffixIcon="/static/icon/my_phone.png"></u--input> -->
+					<!-- 注意：由于兼容性差异，如果需要使用前后插槽，nvue下需使用u--input，非nvue下需使用u-input -->
+					<!-- #ifndef APP-NVUE -->
+					<u-input v-model="formContent.smsCode" prefixIcon="lock"
+						prefixIconStyle="font-size: 22px;color: #909399">>
+						<!-- #endif -->
+						<!-- #ifdef APP-NVUE -->
+						<u--input v-model="formContent.smsCode" prefixIcon="lock"
+							prefixIconStyle="font-size: 22px;color: #909399">>
+							<!-- #endif -->
+							<template slot="suffix">
+								<u-code ref="uCode" @change="codeChange" seconds="20" changeText="秒重新获取"></u-code>
+								<u-button @tap="getCode" :text="tips" type="success" size="mini"></u-button>
+							</template>
+							<!-- #ifndef APP-NVUE -->
+					</u-input>
+					<!-- #endif -->
+					<!-- #ifdef APP-NVUE -->
+					</u--input>
+					<!-- #endif -->
+				</u-form-item>
+				<!-- <view class="btn"> -->
+
+				<!-- </view> -->
+				<view class="remarkForm">
+					<text class="read_tip">登录即表明您已经同意<text class="blue">{{` 《平台用户协议》 `}}</text>和<text
+							class="blue">{{` 《隐私政策》 `}}</text></text>
+				</view>
+				<u-button class="custom-style" :plain="true" @tap="clickSubmit" :hairline="true">登录</u-button>
+
+			</u--form>
+
+		</view>
+
+		<view class="iconbox u-flex u-flex-column u-flex-items-center u-flex-center">
+			<image src="/static/img/login_guarantee.png" mode="aspectFill"></image>
+			<text class="company">弁财，让您的每一次借款都有保障</text>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				formContent: {
+					smsCode: '',
+					phone: ''
+				},
+				tips: '获取验证码',
+				rules: {
+					smsCode: [{
+							required: true,
+							message: '请输入手机验证码',
+							trigger: ['blur', 'change']
+						},
+						{
+							// 自定义验证函数，见上说明
+							validator: (rule, value, callback) => {
+								// 上面有说，返回true表示校验通过，返回false表示不通过
+								return uni.$u.test.code(value, 4)
+							},
+							message: '手机验证码不正确',
+							// 触发器可以同时用blur和change
+							trigger: ['change', 'blur'],
+						}
+					],
+					phone: [{
+							required: true,
+							message: '请输入手机号',
+							trigger: ['change', 'blur'],
+						},
+						{
+							// 自定义验证函数，见上说明
+							validator: (rule, value, callback) => {
+								// 上面有说，返回true表示校验通过，返回false表示不通过
+								// uni.$u.test.mobile()就是返回true或者false的
+								return uni.$u.test.mobile(value);
+							},
+							message: '手机号码不正确',
+							// 触发器可以同时用blur和change
+							trigger: ['change', 'blur'],
+						}
+					]
+				}
+			}
+		},
+		methods: {
+			codeChange(text) {
+				this.tips = text;
+			},
+			getCode() {
+				if (this.$refs.uCode.canGetCode) {
+					// 模拟向后端请求验证码
+					uni.showLoading({
+						title: '正在获取验证码'
+					})
+					setTimeout(() => {
+						uni.hideLoading();
+						// 这里此提示会被this.start()方法中的提示覆盖
+						uni.$u.toast('验证码已发送');
+						// 通知验证码组件内部开始倒计时
+						this.$refs.uCode.start();
+					}, 2000);
+				} else {
+					uni.$u.toast('倒计时结束后再发送');
+				}
+			},
+			change(e) {
+				console.log('change', e);
+			},
+			clickSubmit() {
+				uni.$u.debounce(this.submit, 500)
+			},
+			submit() {
+				console.log('你好')
+				this.$refs.uForm.validate().then(res => {
+					uni.$u.toast('校验通过')
+				}).catch(errors => {
+					uni.$u.toast('校验失败')
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		.login_bg {
+			width: 100%;
+			height: 282px;
+			background: url(../../../static/img/login_bg.png) no-repeat;
+			background-size: cover;
+
+			.header {
+				width: 84px;
+				height: 84px;
+				background: #FFFFFF;
+				border: 4px solid #C0D4FF;
+				border-radius: 84px;
+
+				image {
+					width: 60px;
+					height: 60px;
+				}
+			}
+
+		}
+
+		.formList {
+			height: 279px;
+			background: #FFFFFF;
+			box-shadow: 2px 2px 12px 0px rgba(47, 92, 247, 0.27);
+			border-radius: 8px;
+			margin: -38px 16px 91px 16px;
+			padding: 44px 10px 0 10px;
+			box-sizing: border-box;
+
+
+
+			.remarkForm {
+				margin-top: 20px;
+
+				.read_tip {
+					width: 300px;
+					height: 17px;
+					font-size: 12px;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #414141;
+					line-height: 17px;
+
+					.blue {
+						color: #4579E6;
+					}
+				}
+			}
+
+			/deep/ .u-button--success {
+				border: none;
+				font-size: 12px;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #4579E6;
+				line-height: 17px;
+				background: none;
+			}
+
+			.custom-style {
+				border-radius: 4px;
+				font-size: 16px;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				line-height: 22px;
+				margin-top: 18px;
+				background: #4579E6;
+				color: #FFFFFF;
+			}
+
+		}
+
+		.iconbox {
+			image {
+				width: 100px;
+				height: 100px;
+			}
+
+			.company {
+				font-size: 12px;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #666565;
+				line-height: 17px;
+				margin: 16px 0 65px 0;
+			}
+		}
+	}
+</style>
--- pages/product/evaluationResults/evaluationResults.vue
+++ pages/product/evaluationResults/evaluationResults.vue
@@ -25,6 +25,86 @@
 				</view>
 			</view>
 		</view>
+
+		<view class="remark">
+			工商银行金融服务全面介绍，投资理财信息丰富全面，在线交易方便快捷，满足客户金融服务需求。
+		</view>
+
+		<view class="formData">
+			<u--form :model="formContent" :borderBottom='noneBorder' ref="uForm" labelWidth="auto">
+				<u-form-item label="申请用途" prop="" :borderBottom="noneBorder" @click="showSex = true; hideKeyboard()"
+					ref="item1">
+					<u--input v-model="purpose" disabled inputAlign="right" disabledColor="#ffffff" border="none">
+					</u--input>
+					<u-icon slot="right" name="arrow-right"></u-icon>
+				</u-form-item>
+				<u-form-item label="银行卡号" prop="bankNo" :borderBottom="noneBorder" ref="item1">
+					<u--input inputAlign="right" v-model="formContent.bankNo" border="none"
+						suffixIcon="/static/icon/my_bank.png">
+							</u--input>
+				</u-form-item>
+
+				<u-form-item label="预留手机号" prop="phone" :borderBottom="noneBorder" ref="item1">
+					<u--input inputAlign="right" v-model="phone" border="none" suffixIcon="/static/icon/my_phone.png">
+					</u--input>
+				</u-form-item>
+
+				<u-form-item label="验证码" prop="phone" borderBottom ref="item1">
+					<!-- <u--input inputAlign="right" v-model="phone" border="none" suffixIcon="/static/icon/my_phone.png"></u--input> -->
+					<!-- 注意：由于兼容性差异，如果需要使用前后插槽，nvue下需使用u--input，非nvue下需使用u-input -->
+					<!-- #ifndef APP-NVUE -->
+					<u-input inputAlign="right" v-model="formContent.smsCode" border="none">
+						<!-- #endif -->
+						<!-- #ifdef APP-NVUE -->
+						<u--input inputAlign="right" v-model="formContent.smsCode" border="none">
+							<!-- #endif -->
+							<template slot="suffix">
+								<u-code ref="uCode" @change="codeChange" seconds="20" changeText="X秒重新获取哈哈哈"></u-code>
+								<u-button @tap="getCode" :text="tips" type="success" size="mini"></u-button>
+							</template>
+							<!-- #ifndef APP-NVUE -->
+					</u-input>
+					<!-- #endif -->
+					<!-- #ifdef APP-NVUE -->
+					</u--input>
+					<!-- #endif -->
+				</u-form-item>
+
+				<!-- <u-line></u-line> -->
+
+				<view class="remarkForm">
+					工商银行金融服务全面介绍，投资理财信息丰富全面，在线交易方便快捷，满足客户金融服务需求。
+				</view>
+
+				<view class="read">
+					<u-radio-group v-model="selectRadio">
+						<u-radio shape="square"></u-radio>
+						<text class="read_tip">我已阅读并同意同意<text class="blue">{{` 《会员协议》 `}}</text></text>
+					</u-radio-group>
+				</view>
+
+				<!-- <u-form-item label="银行卡号" prop="bankNo" borderBottom ref="item1">
+<u-radio-group v-model="formContent.selectRadio">
+				<u-radio shape="square"></u-radio>
+				<text class="read_tip">我已阅读并同意同意<text class="blue">{{` 会员协议 `}}</text></text>
+			</u-radio-group>
+				</u-form-item> -->
+
+			</u--form>
+
+			<u-action-sheet :show="showSex" :actions="actions" title="请选择申请用途" description="请选择申请用途"
+				@close="showSex = false" @select="sexSelect">
+			</u-action-sheet>
+
+		</view>
+
+		<view class="btn">
+			<u-button type="primary" :plain="true" class="custom-style" :hairline="true" text="确认">
+			</u-button>
+		</view>
+
+
+
 	</view>
 </template>
 
@@ -53,10 +133,56 @@
 						desc: "24小时专属客服，为用户提供贴心服务。",
 					},
 				],
+				actions: [{
+						name: '男',
+					},
+					{
+						name: '女',
+					},
+					{
+						name: '保密',
+					},
+				],
+				showSex: false,
+				purpose: '',
+				phone: '',
+				formContent: {},
+				selectRadio: false,
+
+				tips: '获取验证码',
+				noneBorder: false,
 			};
 		},
 		onLoad() {},
-		methods: {},
+		methods: {
+			sexSelect(e) {
+				this.purpose = e.name
+			},
+			codeChange(text) {
+				this.tips = text;
+			},
+			getCode() {
+				if (this.$refs.uCode.canGetCode) {
+					// 模拟向后端请求验证码
+					uni.showLoading({
+						title: '正在获取验证码'
+					})
+					setTimeout(() => {
+						uni.hideLoading();
+						// 这里此提示会被this.start()方法中的提示覆盖
+						uni.$u.toast('验证码已发送');
+						// 通知验证码组件内部开始倒计时
+						this.$refs.uCode.start();
+					}, 2000);
+				} else {
+					uni.$u.toast('倒计时结束后再发送');
+				}
+			},
+			change(e) {
+				console.log('change', e);
+			}
+
+		},
 	};
 </script>
 
@@ -98,7 +224,7 @@
 		.advert {
 			width: 343px;
 			height: 171px;
-			background: linear-gradient(180deg, #9ebeff 0%, #ffffff 100%);
+			background: linear-gradient(180deg, #9EBEFF 0%, #FFFFFF 100%);
 			box-shadow: 2px 5px 6px 0px rgba(0, 0, 0, 0.04);
 			border-radius: 4px;
 			margin: 12px 16px;
@@ -154,9 +280,83 @@
 						font-family: PingFangSC-Regular, PingFang SC;
 						font-weight: 400;
 						color: #282626;
-						line-height: 11px;
+						// line-height: 11px;
 					}
 				}
+			}
+		}
+
+		.remark {
+			width: 343px;
+			height: 34px;
+			font-size: 12px;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #414141;
+			line-height: 17px;
+			margin: 12px 16px;
+
+		}
+
+		.formData {
+			margin: 12px 14px;
+			padding: 0 16px;
+
+			.remarkForm {
+				width: 311px;
+				height: 28px;
+				font-size: 10px;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #414141;
+				line-height: 14px;
+				padding: 14px;
+			}
+
+			.read {
+				margin-top: 24px;
+
+				.read_tip {
+
+					font-size: 12px;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #414141;
+					line-height: 17px;
+
+					.blue {
+						color: #4579E6;
+					}
+				}
+			}
+
+			.setImgbox {
+				width: 18px;
+				height: 18px;
+				image {
+					width:100%;
+					height: 100%;
+				}
+			}
+
+		}
+
+		.btn {
+			padding: 0 26px;
+			margin-top: 28px;
+			width: 323px;
+			height: 44px;
+			position: fixed;
+			bottom: 0;
+
+			.custom-style {
+				background: #4579E6;
+				border-radius: 4px;
+				font-size: 16px;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #FFFFFF;
+				line-height: 22px;
 			}
 		}
 	}
