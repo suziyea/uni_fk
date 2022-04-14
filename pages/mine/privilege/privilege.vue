e4f92a8b8c815574b434a84164f6123e0102bd1b
houakang <houakang@126.com>
2022-04-14 13:28:16 +0800

    feat: 我的权益

---
+++ pages/mine/privilege/privilege.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="container" :class="addClassName">
+		<view class="empty" v-if="emptyStatus">
+			<u-empty mode="order" :text="emptyText" icon="http://cdn.uviewui.com/uview/empty/order.png">
+			</u-empty>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				emptyStatus: true,
+				emptyText: '暂无权益'
+			}
+		},
+		methods: {
+
+		},
+		computed: {
+			addClassName() {
+				if (this.emptyStatus) {
+					return 'u-flex u-flex-column u-flex-center u-flex-items-center'
+				}
+				return ''
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		width: 100%;
+		min-height: 100vh;
+	}
+</style>
